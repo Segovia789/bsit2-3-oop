5078d58c4bf0d01440ad2fd4112fd58bc915976a
argie segovia <segoviaa13872@liceo.edu.ph>
2025-09-15 15:40:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test Branch");
-        }
+public class Manager extends Employee {
+    private double bonus;
+    private int teamSize;
+
+    public Manager(String name, int employeeId, double baseSalary, String department, double bonus, int teamSize) {
+        super(name, employeeId, baseSalary, department); // Call parent constructor
+        this.bonus = bonus;
+        this.teamSize = teamSize;
+        System.out.println(name + " has been promoted to Manager");
     }
+
+    @Override
+    public double calculateSalary() {
+        return super.calculateSalary() + bonus; // Base + Bonus
+    }
+
+    @Override
+    public void work() {
+        super.work(); // Call parent work method
+        System.out.println(name + " is managing a team of " + teamSize + " employees");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo(); // Display parent info
+        System.out.println("Bonus: $" + bonus);
+        System.out.println("Team Size: " + teamSize);
+    }
+}
