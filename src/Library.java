e6c326fe8f98513cb0bbd13466f3d1152dc971fd
argie segovia <segoviaa13872@liceo.edu.ph>
2025-09-22 15:52:47 +0800

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,1 +1,73 @@
+public class Library {
+    private Book[] books;
+    private int bookCount;
 
+    public Library() {
+        books = new Book[10];
+        bookCount = 0;
+    }
+
+    public boolean addBook(Book book) {
+        if (bookCount < books.length) {
+            books[bookCount++] = book;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeBook(String isbn) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getIsbn().equals(isbn)) {
+                books[i] = books[bookCount - 1]; // Replace with last
+                books[--bookCount] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Book findBook(String isbn) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getIsbn().equals(isbn)) {
+                return books[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean borrowBook(String isbn) {
+        Book book = findBook(isbn);
+        if (book != null) {
+            return book.borrowBook();
+        }
+        return false;
+    }
+
+    public boolean returnBook(String isbn) {
+        Book book = findBook(isbn);
+        if (book != null) {
+            return book.returnBook();
+        }
+        return false;
+    }
+
+    public void displayAvailableBooks() {
+        System.out.println("Available books:");
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].isAvailable()) {
+                books[i].displayBookInfo();
+            }
+        }
+    }
+
+    public void displayAllBooks() {
+        System.out.println("All books in library:");
+        for (int i = 0; i < bookCount; i++) {
+            books[i].displayBookInfo();
+        }
+    }
+
+    public int getBookCount() {
+        return bookCount;
+    }
+}
