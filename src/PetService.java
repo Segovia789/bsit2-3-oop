7e5bf28c8ae19942543fc5cd530d317738900ae0
argie segovia <segoviaa13872@liceo.edu.ph>
2025-09-23 00:05:03 +0800

    Update PetService.java

--- src/PetService.java
+++ src/PetService.java
@@ -3,12 +3,10 @@ class PetService {
     private static final double VACCINATION_FEE = 25.0;
     private static final double GROOMING_FEE = 30.0;
 
-    // Basic checkup
     public double calculateFee() {
         return BASE_FEE;
     }
 
-    // With vaccination
     public double calculateFee(boolean withVaccination) {
         if (withVaccination) {
             return BASE_FEE + VACCINATION_FEE;
@@ -16,7 +14,6 @@ class PetService {
         return BASE_FEE;
     }
 
-    // With vaccination + grooming
     public double calculateFee(boolean withVaccination, boolean withGrooming) {
         double total = BASE_FEE;
         if (withVaccination) total += VACCINATION_FEE;
@@ -24,7 +21,6 @@ class PetService {
         return total;
     }
 
-    // Emergency
     public double calculateFee(String emergencyType) {
         return 200.0;
     }
